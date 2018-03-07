97d550cb3b0030fe12a33f9d330e21057821493f
JasonSooter <jason.sooter@gmail.com>
2018-03-07 12:43:12 -0600

    Added dispatch method to createStore

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false,
-  }
-}
+// {
+//   type: 'REMOVE_TODO',
+//   id: 0,
+// }
 
-{
-  type: 'REMOVE_TODO',
-  id: 0,
-}
+// {
+//   type: 'TOGGLE_TODO',
+//   id: 0,
+// }
 
-{
-  type: 'TOGGLE_TODO',
-  id: 0,
-}
+// {
+//   type: 'ADD_GOAL',
+//   goal: {
+//     id: 0,
+//     name: 'Run a Marathon'
+//   }
+// }
 
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
+// {
+//   type: 'REMOVE_GOAL',
+//   id: 0
+// }
 
-{
-  type: 'REMOVE_GOAL',
-  id: 0
-}
+// {
+//   type: 'ADD_TODO',
+//   todo: {
+//     id: 0,
+//     name: 'Learn Redux',
+//     complete: false,
+//   }
+// }
 
 /*
 Characteristics of a Pure Function
@@ -38,43 +38,43 @@ Characteristics of a Pure Function
 */
 
 // Reducer function
-function todos (state = [], action) {
+function todos(state = [], action) {
   if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+    return state.concat([action.todo]);
   }
 
-  return state
+  return state;
 }
 
-function createStore (reducer) {
+function createStore(reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state. (getState)
   // 3. Listen to changes on the state. (subscribe)
   // 4. Update the state (dispatch)
 
-  let state
-  let listeners = []
+  let state;
+  let listeners = [];
 
-  const getState = () => state
+  const getState = () => state;
 
-  const subscribe = (listener) => {
-    listeners.push(listener)
+  const subscribe = listener => {
+    listeners.push(listener);
     return () => {
-      listeners = listeners.filter((l) => l !== listener)
-    }
-  }
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
 
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
 
   return {
     getState,
     subscribe,
-    dispatch,
-  }
+    dispatch
+  };
 }
 
-const store = createStore(todos)
+const store = createStore(todos);
