a3a095c45d695b8a86444f2c0939a34f825eda4c
JasonSooter <jason.sooter@gmail.com>
2018-03-07 15:30:07 -0600

    Added goals reducer and combining(root) reducer

--- index.js
+++ index.js
@@ -17,97 +17,93 @@ function todos(state = [], action) {
   }
 }
 
-function goals (state = [], action) {
-  switch(action.type) {
-    case 'ADD_GOAL' :
-      return state.concat([action.goal])
-    case 'REMOVE_GOAL' :
-      return state.filter((goal) => goal.id !== action.id)
-    default :
-      return state
+function goals(state = [], action) {
+  switch (action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal]);
+    case 'REMOVE_GOAL':
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
   }
 }
 
-function app (state = {}, action) {
+function app(state = {}, action) {
   return {
     todos: todos(state.todos, action),
     goals: goals(state.goals, action)
-  }
+  };
 }
 
-function createStore (reducer) {
+function createStore(reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state. (getState)
   // 3. Listen to changes on the state. (subscribe)
   // 4. Update the state (dispatch)
 
-  let state
-  let listeners = []
+  let state;
+  let listeners = [];
 
-  const getState = () => state
+  const getState = () => state;
 
-  const subscribe = (listener) => {
-    listeners.push(listener)
+  const subscribe = listener => {
+    listeners.push(listener);
     return () => {
-      listeners = listeners.filter((l) => l !== listener)
-    }
-  }
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
 
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
 
   return {
     getState,
     subscribe,
-    dispatch,
-  }
+    dispatch
+  };
 }
 
-const store = createStore(app)
+const store = createStore(app);
 
-store.subscribe(() => {
-  console.log('The new state is: ', store.getState())
-})
+store.subscribe(() => console.log('The new state is: ', store.getState()));
 
 store.dispatch({
   type: 'ADD_TODO',
   todo: {
     id: 0,
     name: 'Walk the dog',
-    complete: false,
+    complete: false
   }
-})
-
+});
 store.dispatch({
   type: 'ADD_TODO',
   todo: {
     id: 1,
     name: 'Wash the car',
-    complete: false,
+    complete: false
   }
-})
-
+});
 store.dispatch({
   type: 'ADD_TODO',
   todo: {
     id: 2,
     name: 'Go to the gym',
-    complete: true,
+    complete: false
   }
-})
+});
 
 store.dispatch({
   type: 'REMOVE_TODO',
   id: 1
-})
+});
 
 store.dispatch({
   type: 'TOGGLE_TODO',
   id: 0
-})
+});
 
 store.dispatch({
   type: 'ADD_GOAL',
@@ -115,17 +111,17 @@ store.dispatch({
     id: 0,
     name: 'Learn Redux'
   }
-})
+});
 
 store.dispatch({
   type: 'ADD_GOAL',
   goal: {
     id: 1,
-    name: 'Lose 20 pounds'
+    name: 'Lose 20 lbs'
   }
-})
+});
 
 store.dispatch({
   type: 'REMOVE_GOAL',
   id: 0
-})
+});
