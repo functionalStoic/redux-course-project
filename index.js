3afc153bd56857c392e28d510e68a15d81a819f1
JasonSooter <jason.sooter@gmail.com>
2018-03-07 13:10:29 -0600

    Added more actions to reducer

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Reducer function
-function todos (state = [], action) {
-  switch(action.type) {
-    case 'ADD_TODO' :
-      return state.concat([action.todo])
-    case 'REMOVE_TODO' :
-      return state.filter((todo) => todo.id !== action.id)
-    case 'TOGGLE_TODO' :
-      return state.map((todo) => todo.id !== action.id ? todo :
-        Object.assign({}, todo, {complete: !todo.complete})
-      )
-    default :
-      return state
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id !== action.id);
+    case 'TOGGLE_TODO':
+      return state.map(
+        todo =>
+          todo.id !== action.id
+            ? todo
+            : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
   }
 }
 
