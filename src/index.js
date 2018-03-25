a9900d9a9fd9d4ba9556a29caa17b88d1e4bd604
JasonSooter <jason.sooter@gmail.com>
2018-03-25 17:52:45 -0500

    Moved store creation into index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 
+import { createStore } from 'redux';
+import reducer from './reducers/';
+import middleware from './middleware/';
+
+import { Provider } from 'react-redux';
 import App from './components/App';
-import store from './store';
+
+const store = createStore(reducer, middleware);
 
 render(
   <Provider store={store}>
--- src/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import reducers from './reducers/';
-import middleware from './middleware/';
-
-export default createStore(reducers, middleware);
